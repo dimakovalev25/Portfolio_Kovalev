dd1852f13aca2fb82f28b9dff5251e05580b1e72
dimakovalev25 <d.nozdryn@gmail.com>
2022-07-18 22:32:05 +0300

    portfolio 6section

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,25 @@ hamburger.addEventListener('click', () => {     /* отслеживаем кли
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+
+
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    
+    if (!$(this).valid()) {
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
